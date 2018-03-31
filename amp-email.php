335182cc2635b3a26b8fba667c36afb56c9e2653
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-03-31 22:38:54 +0700

    Create amp-email.php

---
+++ amp-email.php
@@ -0,0 +1,39 @@
+<?php
+$to = "handyman.shirts@gmail.com";
+$subject = "HTML email";
+
+$message = "
+<!doctype html>
+<html ⚡4email> <!-- `amp4email` also accepted. -->
+<head>
+  <title>AMP HTML email</title>
+  <meta charset="utf-8">
+  <style amp4email-boilerplate>body{visibility:hidden}</style>
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <info@onetotech.com>' . "\r\n";
+$headers .= 'Cc: anissunaryati@gmail.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>

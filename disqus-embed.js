865583ab1498837350eb72bbdb16a8ee973f6518
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-06-07 20:13:07 +0700

    Update disqus-embed.js

--- disqus-embed.js
+++ disqus-embed.js
@@ -1,14 +1,28 @@
-var disqus_shortname = &quot;berikabar&quot;;
-var disqus_url = &quot;<data:blog.canonicalUrl/>&quot;;
-var disqus_identifier = &quot;<data:blog.canonicalUrl/>&quot;; 
+// Replace labnol with your disqus shortname
+var disqus_shortname = "berikabar";
+
+// Put the permalink of your web page / blog post
+var disqus_url = "<data:blog.canonicalUrl/>";
+
+// Put the permalink of your web page / blog post
+var disqus_identifier = "<data:blog.canonicalUrl/>"; 
+
 var disqus_loaded = false;
+
+// This is the function that will load Disqus comments on demand
 function disqus() {
-if (!disqus_loaded)  {
-disqus_loaded = true;
-var e = document.createElement(&quot;script&quot;);
-e.type = &quot;text/javascript&quot;;
-e.async = true;
-e.src = &quot;//&quot; + disqus_shortname + &quot;.disqus.com/embed.js&quot;;
-(document.getElementsByTagName(&quot;head&quot;)[0] ||
-document.getElementsByTagName(&quot;body&quot;)[0])
-.appendChild(e);}}
+
+  if (!disqus_loaded)  {
+    
+    // This is to ensure that Disqus widget is loaded only once
+    disqus_loaded = true;
+    
+    var e = document.createElement("script");
+    e.type = "text/javascript";
+    e.async = true;
+    e.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+    (document.getElementsByTagName("head")[0] ||
+     document.getElementsByTagName("body")[0])
+    .appendChild(e);
+  }
+} 

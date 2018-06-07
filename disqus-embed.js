20d1e051835081664b42ec501efb56e9f7f8cb2a
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-06-07 20:07:36 +0700

    Create disqus-embed.js

---
+++ disqus-embed.js
@@ -0,0 +1,14 @@
+var disqus_shortname = &quot;berikabar&quot;;
+var disqus_url = &quot;<data:blog.canonicalUrl/>&quot;;
+var disqus_identifier = &quot;<data:blog.canonicalUrl/>&quot;; 
+var disqus_loaded = false;
+function disqus() {
+if (!disqus_loaded)  {
+disqus_loaded = true;
+var e = document.createElement(&quot;script&quot;);
+e.type = &quot;text/javascript&quot;;
+e.async = true;
+e.src = &quot;//&quot; + disqus_shortname + &quot;.disqus.com/embed.js&quot;;
+(document.getElementsByTagName(&quot;head&quot;)[0] ||
+document.getElementsByTagName(&quot;body&quot;)[0])
+.appendChild(e);}}
